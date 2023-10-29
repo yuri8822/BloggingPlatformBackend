2f60e346a36227cdfe8d3341e36d4b4943ba98ca
Umar.exe <61727014+yuri8822@users.noreply.github.com>
2023-10-29 17:43:05 +0500

    wrote starter code for app.js and created initial files for models and router

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = require('./routes');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/WEB_ENG', { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => { console.log('Connected to MongoDB Successfully') });
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', router);
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
